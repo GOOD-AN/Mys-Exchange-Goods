2a251361fee57bea2c67a177b3b599ce2d8256b2
GOOD-AN <53306377+GOOD-AN@users.noreply.github.com>
2023-05-29 18:37:51 +0800

    fix: 修复输出信息错误问题

--- myseg/tocli/__init__.py
+++ myseg/tocli/__init__.py
@@ -7,11 +7,11 @@ from myseg.global_var import user_global_var as gl
 
 result = gl.load_config()
 if result != 101:
-    print(f"配置文件加载失败, 错误为: {result}")
+    print(f"配置文件加载失败, 错误为: {result[1]}")
     input("按回车键继续")
     sys.exit()
 result = gl.check_plat()
 if result != 101:
-    print(f"平台检测失败, 错误为: {result}")
+    print(f"平台检测失败, 错误为: {result[1]}")
     input("按回车键继续")
     sys.exit()
